3c7d2cfbfae3cd372b9b8b8bc427127e94635df4
yizzy__ <169167871+yizzy075@users.noreply.github.com>
2025-11-01 18:19:08 -0500

    Update StringValueIsPresentRule.java

--- ApiNose/ApiNose/src/main/java/co/edu/uco/nose/business/business/rule/generics/StringValueIsPresentRule.java
+++ ApiNose/ApiNose/src/main/java/co/edu/uco/nose/business/business/rule/generics/StringValueIsPresentRule.java
@@ -6,31 +6,23 @@ import co.edu.uco.nose.crosscuting.helper.TextHelper;
 
 public class StringValueIsPresentRule implements Rule {
     @Override
-    public void execute(Object... data) {
+    public void execute(final Object... data) {
         if (ObjectHelper.isNull(data)) {
-            var userMessage = "No se ha presentado un problema inesperado tratando de llevar a cabo la operacion deseada....";
-            var technicalMessage = "No se recibieron los parametros requeridos para ejecutar la regla StringValueIsPresentRule";
-            throw NoseException.create(userMessage, technicalMessage);
-        }
-        if (data.length < 2) {
-            var userMessage = "se ha presentado un problema tratanddo de llevar acabo la operacion deseada....";
-            var technicalMessage = " se recibieron dos parametros y llego una cantidad menos a esta requeridos para ejecutar la regla StringValueIsPresentRule";
-            throw NoseException.create(userMessage, technicalMessage);
-        }
-        if(data.length<3) {
-            var userMessage = "se ha presentado un problema tratanddo de llevar acabo la operacion deseada....";
-            var technicalMessage = " se recibieron dos parametros y llego una cantidad menos a esta requeridos para ejecutar la regla StringValueIsPresentRule";
+            var userMessage = "Se ha presentado un problema inesperado tratando de llevar a cabo la operacion deseada....";
+            var technicalMessage = "No se recibieron los parametros requeridos para ejecutar la regla stringValueIsPresentRule";
             throw NoseException.create(userMessage, technicalMessage);
 
         }
+
+        if (data.length < 3) {
+            var userMessage = "Se ha presentado un problema inesperado tratando de llevar a cabo la operacion deseada....";
+            var technicalMessage = "Se requerian tres parametros y llego una cantidad menor a esta requeridos para ejecutar la regla StringValueIsPresentRule";
+            throw NoseException.create(userMessage, technicalMessage);
+
+        }
+
         var stringData = (String) data[0];
         var dataName = (String) data[1];
-        boolean mustApplyTrim = (boolean) data[2];
-
-        if(mustApplyTrim) ? TextHelper.isEmptyWithTrim(stringData): TextHelper.isEmptyWithTrim(dataName); {
-            var userMessage = "";
-            var technicalMessage = " se recibieron dos parametros y llego una cantidad menos a esta requeridos para ejecutar la regla StringValueIsPresentRule";
-            throw NoseException.create(userMessage, technicalMessage);
+        var mustApplyTrim = (boolean) data[2];
     }
-
 }
