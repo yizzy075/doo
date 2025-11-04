c402ef2fea8dd123b4f4161f48fd269f7cbb1719
yizzy__ <169167871+yizzy075@users.noreply.github.com>
2025-11-03 21:47:02 -0500

    a probar porq bruno no da

--- ApiNose/src/main/java/co/edu/uco/nose/dto/CityDTO.java
+++ ApiNose/src/main/java/co/edu/uco/nose/dto/CityDTO.java
@@ -2,6 +2,7 @@ package co.edu.uco.nose.dto;
 
 import java.util.UUID;
 
+ frontend
 import co.edu.uco.nose.crosscuting.helpers.ObjectHelper;
 import co.edu.uco.nose.crosscuting.helpers.TextHelper;
 import co.edu.uco.nose.crosscuting.helpers.UUIDHelper;
@@ -58,4 +59,39 @@ public final class CityDTO {
     public void setState(final StateDTO state) {
         this.state = ObjectHelper.getDefault(state, StateDTO.getDefaultObject());
     }
+
+import co.edu.uco.nose.crosscuting.helper.TextHelper;
+import co.edu.uco.nose.crosscuting.helper.UUIDHelper;
+
+public class CityDTO extends DTO{
+	
+	private String name;
+	
+	
+	
+	public CityDTO() {
+		super(UUIDHelper.getUUIDHelper().getDefault());
+		setName(TextHelper.getDefault());
+	}
+
+	public CityDTO(final UUID id) {
+		super(id);
+		setName(TextHelper.getDefault());
+	}
+	
+	public CityDTO(final UUID id,final String name) {
+		super(id);
+		this.name = name;
+	}
+
+	
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = TextHelper.getDefaultWithTrim(name);
+	}main
 }
